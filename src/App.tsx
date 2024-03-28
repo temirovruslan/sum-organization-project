ae7ca1df2e4f4c7e68117f96b6e4d8c6716c99b5
Temirov Ruslan <ruslan.temirov1995@gmail.com>
2024-03-28 10:08:17 +0200

    first

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className="fixed top-0 right-0 bottom-0 left-0 overflow-hidden z-0 flex justify-center items-center">
+    <img src="../public/main.png" alt="" className="w-full h-full object-cover" />
+</div>
+
+	);
+}
+
+export default App;
